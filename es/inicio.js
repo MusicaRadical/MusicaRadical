eb62b4af4aef7b2d11782cfb1c83afe40fdb62d9
AlexisDominguez <alexisdominguezsanchez3@gmail.com>
2018-05-28 21:51:29 -0600

    submit 1.2 Añadidas animaciones, reconfiguracion del header. paneles sticky.

---
+++ es/inicio.js
@@ -0,0 +1,15 @@
+
+/* .panel-izquierda sticky */
+/*$(document).ready(function(){
+
+	var altura = $('.panel-izquierda').offset().top;
+
+	$(window).on('scroll', function(){
+		if ($(window).scrollTop() > altura){
+			$('.panel-izquierda').addClass('panel-izquierda-fixed');
+		}else{
+			$('.panel-izquierda').removeClass('panel-izquierda-fixed');
+		}
+	})
+	
+})*/
